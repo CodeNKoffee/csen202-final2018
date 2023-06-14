f9a0628b80f9ce4d65a0501ba46578763a56a28c
Hatem Soliman <hatemthedev@gmail.com>
2023-06-14 20:52:16 +0300

    First commit - Exercise 1

---
+++ one.java
@@ -0,0 +1,22 @@
+public class one {
+  public static void main(String[] args) {
+    int len = args.length;
+    int n = Integer.parseInt(args[0]);
+    int[] x = new int[len - 1];
+    for (int i = 1; i < args.length; i++) {
+      x[i - 1] = Integer.parseInt(args[i]);
+    }
+    sumArray(x, n);
+  }
+
+  // Question 1A
+  public static void sumArray(int[] arr, int n) {
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = i + 1; j < arr.length; j++) {
+        if (arr[j] + arr[i] == n) {
+          System.out.println(arr[j] +" + "+ arr[i] +" = "+ n);
+        }
+      }
+    }
+  }
+} 
